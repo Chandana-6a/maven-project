09fe7fa4d667669b2e818d8ec377efb7cf38e14f
azuredevops7 <azuredevopsshammu@gmail.com>
2026-01-07 18:46:01 +0530

    Create sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,4 @@
+       mvn sonar:sonar \
+                      -Dsonar.projectKey=maven \
+                      -Dsonar.host.url=http://13.239.1.158:9000 \
+                      -Dsonar.login=3580df05ddd9a280b47d01922f8abdb707a46e05
