f5f4479d2302ffe8380aa130ad8b7b01058cc223
Chandana <bolshetty.chandana@gmail.com>
2026-01-08 19:15:09 +0530

    Update ecr.sh

--- ecr.sh
+++ ecr.sh
@@ -1,4 +1,4 @@
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 574070665112.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t proj2-ecr .
-docker tag proj2-ecr:latest 574070665112.dkr.ecr.ap-south-1.amazonaws.com/proj2-ecr:latest
-docker push 574070665112.dkr.ecr.ap-south-1.amazonaws.com/proj2-ecr:latest
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 574070665112.dkr.ecr.eu-west-2.amazonaws.com
+docker build -t practiceproject .
+docker tag practiceproject:latest 574070665112.dkr.ecr.eu-west-2.amazonaws.com/practiceproject:latest
+docker push 574070665112.dkr.ecr.eu-west-2.amazonaws.com/practiceproject:latest
